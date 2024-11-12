82af1c80aa0a7b22ab8f5214200e8efdea7ce888
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-12 23:24:28 +0000

    tailwind

--- proj/frontend/src/App.tsx
+++ proj/frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './output.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <h1 className='text-red-500'>Olá</h1>
+      <div className='bg-yellow-200'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Olá</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
