f7f6270bfa095c61e282b86bb614ee25724a07d0
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-13 15:38:25 +0000

    now tailwind is working

--- proj/frontend/src/Layout.tsx
+++ proj/frontend/src/Layout.tsx
@@ -1,22 +1,39 @@
 import React from "react";
+import "./index.css";
+import "./output.css";
+
+
+const SideBarLayout = () => {
+    return (
+        <aside className="fixed top-0 left-0 w-64 h-full bg-white p-4 shadow-md">
+            <h2 className="text-lg text-red-600 font-bold mb-4">Menu</h2>
+            <ul>
+                <li className="mb-2 hover:text-gray-300">Item 1</li>
+                <li className="mb-2 hover:text-gray-300">Item 2</li>
+                <li className="mb-2 hover:text-gray-300">Item 3</li>
+            </ul>
+        </aside>
+    );
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="min-h-screen flex flex-col">
-            {/* Cabeçalho */}
-            <header className="bg-gray-800 text-white p-4">
-                <h1 className="text-2xl">Meu Site</h1>
-            </header>
+        <>
+            <div className="flex-1">
+                <SideBarLayout />
+            </div>
 
-            {/* Conteúdo */}
-            <main className="flex-grow p-4">{children}</main>
+            <div className="ml-64">
+                <main className="flex-grow p-4 bg-gray-100">
+                    {children}
+                </main>
+                <footer className="bg-gray-800 text-white p-4 text-center">
+                    &copy; 2024 Meu Site. Todos os direitos reservados.
+                </footer>
+            </div>
+        </>
 
-            {/* Rodapé */}
-            <footer className="bg-gray-800 text-white p-4 text-center">
-                &copy; 2024 Meu Site. Todos os direitos reservados.
-            </footer>
-        </div>
     );
-}
+};
 
 export default Layout;
