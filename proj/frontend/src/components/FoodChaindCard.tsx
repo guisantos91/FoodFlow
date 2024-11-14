4f3fd765c539da1c7d961eeb8290962f82dcd791
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-14 10:47:01 +0000

    card component update

--- proj/frontend/src/components/FoodChaindCard.tsx
+++ proj/frontend/src/components/FoodChaindCard.tsx
@@ -1,8 +1,20 @@
+import MCImage from '../assets/images/logos/mcdonalds.png';
+
 const FoodChainCard = () => {
     return (
-        <div className="w-96 h-40 bg-orange-400 rounded-xl">
-            <h1>Olá</h1>
-        </div>
+        <>
+            <div className="w-96 h-40 bg-orange-500 rounded-2xl flex">
+                <div className="bg-white w-32 h-full rounded-l-2xl">
+                    <img src={MCImage} alt="McDonalds" className="w-full h-full object-cover rounded-l-2xl" />
+                </div>
+                <div className="flex flex-col justify-center p-4 ml-7">
+                    <h1 className="text-3xl font-extrabold text-green-800">McDonald's</h1>
+                    <button className="mt-7 px-4 py-2 bg-green-800 text-white text-lg font-semibold rounded-xl hover:bg-green-600">
+                        See More
+                    </button>
+                </div>
+            </div>
+        </>
     );
 }
 
