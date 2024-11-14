e371a3784fa344f970bffd633fab46ff975cac3c
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-14 10:02:33 +0000

    card food chain component

--- proj/frontend/src/HomePage.tsx
+++ proj/frontend/src/HomePage.tsx
@@ -1,7 +1,18 @@
-
+import FoodChainCard from "./components/FoodChaindCard";
+import Layout from "./Layout";
 
 const HomePage = () => {
-    return <h1 className="text-2xl">Home Page</h1>;
+    return (
+        <Layout>
+            <div className="flex justify-center h-screen">
+                <div className="text-center">
+                    <h2 className="text-2xl mb-4">User Page</h2>
+                    <p>User page url</p>
+                    <FoodChainCard />
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default HomePage;
---
+++ proj/frontend/src/components/FoodChaindCard.tsx
@@ -0,0 +1,9 @@
+const FoodChainCard = () => {
+    return (
+        <div className="w-96 h-40 bg-orange-400 rounded-xl">
+            <h1>Olá</h1>
+        </div>
+    );
+}
+
+export default FoodChainCard;
