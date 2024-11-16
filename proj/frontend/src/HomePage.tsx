e8266a45690b8c20ad587e97f8dfb1c392d6a9f6
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-16 16:15:42 +0000

    margin bottom resolved

--- proj/frontend/src/HomePage.tsx
+++ proj/frontend/src/HomePage.tsx
@@ -24,11 +24,11 @@ const HomePage: React.FC = () => {
     return (
         <Layout>
             <div className="flex min-h-screen">
-                <div className="flex-1 flex justify-center">
+                <div className="flex-1 flex justify-center bg-white">
                     <div className="text-center">
                         <h2 className="text-2xl mb-4">User Page</h2>
                         <p>User page url</p>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-8 mb-8">
                             {foodChains.map((chain, index) => (
                                 <FoodChainCard key={index} name={chain.name} image={chain.image} />
                             ))}
--- proj/frontend/src/components/Cards/FoodChaindCard.tsx
+++ proj/frontend/src/components/Cards/FoodChaindCard.tsx
@@ -7,22 +7,22 @@ interface FoodChainCardProps {
 
 const FoodChainCard: React.FC<FoodChainCardProps> = ({ name, image }) => {
     return (
-        <div className="w-96 h-40 bg-orange-500 rounded-2xl flex border-4 border-orange-500 m-8">
-            {/* Container da imagem */}
-            <div className="bg-white w-36 h-full rounded-l-2xl flex items-center justify-center p-2">
+        <div className="card card-side bg-orange-500 shadow-xl">
+            <figure className="w-36 h-36 flex items-center justify-center">
                 <img
                     src={image}
                     alt={name}
-                    className="max-w-full max-h-full object-contain rounded-2xl"
+                    className="w-32 h-32 object-contain rounded-xl bg-white p-2"
                 />
-            </div>
-            <div className="flex flex-col justify-center p-4 ml-7">
-                <h1 className="text-2xl font-extrabold text-white">{name}</h1>
-                <button
-                    className="mt-4 w-28 h-10 bg-green-800 text-white text-lg font-semibold rounded-xl hover:bg-green-600"
-                >
-                    See More
-                </button>
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title text-white">{name}</h2>
+                <div className="card-actions justify-end">
+                    <button className="bg-green-800 text-white text-lg font-bold px-4 py-2 rounded-lg hover:bg-green-700 -mb-3 mt-3">
+                        See More
+                    </button>
+
+                </div>
             </div>
         </div>
     );
