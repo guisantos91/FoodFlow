696fee230bdeceb2a479824e3db32d30f2c08575
Joao <joaomonteiro29@ua.pt>
2024-11-27 18:22:41 +0000

    add endpoint for specific form

--- proj/backend/src/main/java/com/ua/ies/proj/app/repos/ManagerFormRepository.java
+++ proj/backend/src/main/java/com/ua/ies/proj/app/repos/ManagerFormRepository.java
@@ -1,8 +1,11 @@
 package com.ua.ies.proj.app.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ua.ies.proj.app.models.ManagerForm;
 
 public interface ManagerFormRepository extends JpaRepository<ManagerForm, Long> {
+    Optional<ManagerForm> findById(Long id);
 }
