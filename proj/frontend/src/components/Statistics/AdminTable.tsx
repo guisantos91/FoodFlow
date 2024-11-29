2e9daa0ae668271732075f51d2d6d77606b8a5a7
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-11-29 14:59:32 +0000

    table updates

--- proj/frontend/src/components/Statistics/AdminTable.tsx
+++ proj/frontend/src/components/Statistics/AdminTable.tsx
@@ -1,6 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
+import McImage from "../../assets/images/logos/mcdonalds.png";
+
+interface Manager {
+    id: number;
+    fname: string;
+    lname: string;
+}
+
+interface FoodChain {
+    id: number;
+    name: string;
+}
+
+interface Restaurant {
+    id: number;
+    name: string;
+    foodchain: FoodChain;
+    manager: number | null;
+}
 
 const AdminTable: React.FC = () => {
+    const [managers, setManagers] = React.useState<Manager[]>([]);
+    const [restaurants, setRestaurants] = React.useState<Restaurant[]>([]);
+
+    useEffect(() => {
+        const fetchManagers = async () => {
+            try {
+                const baseUrl = 'http://localhost:8080/api/v1/admin';
+                const response = await axios.get(`${baseUrl}/managers `, { //O espaço e, `${baseUrl}/managers ` é obrigatório
+                    withCredentials: true,
+                });
+                setManagers(response.data)
+                console.log(response.data);
+            } catch (error) {
+                console.error("Failed to fetch managers: ", error);
+            }
+        };
+        fetchManagers();
+    }, []);
+
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            try {
+                const response = await axios.get(
+                    'http://localhost:8080/api/v1/foodchains/restaurants'
+                );
+                const restaurantsWithDistance = response.data.map((restaurant: Restaurant) => {
+                    return { ...restaurant, manager: 2 };   // change later
+                });
+                setRestaurants(restaurantsWithDistance);
+                //   setRestaurants(response.data);
+                console.log("Restaurants Data:", response.data);
+                console.log("Restaurants Data2:", restaurantsWithDistance);
+
+            } catch (err) {
+                console.error("Error fetching Restaurants:", err);
+            }
+        };
+
+        fetchRestaurants();
+    }, []);
+
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-9/12">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -18,39 +79,22 @@ const AdminTable: React.FC = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" className="px-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-14 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Laptop
-                        </td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" className="px-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-14 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            White
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Laptop PC
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row" className="px-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-14 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Black
-                        </td>
-                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Accessories
-                        </td>
-                    </tr>
+                    {restaurants.map((restaurant) => (
+                        <tr
+                            key={restaurant.id}
+                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                        >
+                            <td className="px-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {managers.find((manager) => manager.id === restaurant.manager)?.fname}
+                            </td>
+                            <td className="px-14 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {restaurant.name}
+                            </td>
+                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Olá
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
