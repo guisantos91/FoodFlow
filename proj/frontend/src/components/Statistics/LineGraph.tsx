4120d8e44006baba0327bebea89c065a3e2b2d09
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-12-09 15:29:46 +0000

    hours and minutes

--- proj/frontend/src/components/Statistics/LineGraph.tsx
+++ proj/frontend/src/components/Statistics/LineGraph.tsx
@@ -1,4 +1,4 @@
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface LineGraphData {
     name: string;
@@ -12,8 +12,11 @@ interface LineGraphProps {
 
 
 function LineGraph({ data, colorMapping }: LineGraphProps) {
+
+    const now = new Date();
+
     const transformedData = data[0]?.values.map((_, index) => {
-        const point: { [key: string]: number | string } = { name: `Minute ${index}` };
+        const point: { [key: string]: number | string } = { name: new Date(now.getTime() + index * 60 * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) };
         data.forEach((item) => {
             point[item.name] = item.values[index];
         });
@@ -30,7 +33,7 @@ function LineGraph({ data, colorMapping }: LineGraphProps) {
                     <Tooltip />
                     <Legend />
                     {data.map((item) => (
-                        <Line key={item.name} type="monotone" dataKey={item.name} stroke={colorMapping[item.name]}/>
+                        <Line key={item.name} type="monotone" dataKey={item.name} stroke={colorMapping[item.name]} />
                     ))}
                 </LineChart>
             </ResponsiveContainer>
