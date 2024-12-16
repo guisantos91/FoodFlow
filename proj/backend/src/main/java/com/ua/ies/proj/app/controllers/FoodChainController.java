497b84de66afd99c40739fa619e87688a77d0360
Gui113893 <gui.santos91@ua.pt>
2024-12-16 12:10:25 +0000

    foodchain controller

--- proj/backend/src/main/java/com/ua/ies/proj/app/controllers/FoodChainController.java
+++ proj/backend/src/main/java/com/ua/ies/proj/app/controllers/FoodChainController.java
@@ -18,8 +18,17 @@ import com.ua.ies.proj.app.models.Restaurant;
 import com.ua.ies.proj.app.services.OrderService;
 import com.ua.ies.proj.app.services.RestaurantsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/foodchains")
+@Tag(name = "Foodchain", description = "Foodchain operations")
 public class FoodChainController {
     @Autowired
     private final RestaurantsService restaurantsService;
@@ -32,46 +41,131 @@ public class FoodChainController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "Get all foodchains")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Foodchains found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Foodchain.class)
+            )
+        )
+    })
     @GetMapping("/")
     public ResponseEntity<List<Foodchain>> getFoodchains() {
         List<Foodchain> foodchains = restaurantsService.getFoodchains();
         return new ResponseEntity<>(foodchains, HttpStatus.OK);
     }
 
-    
-
+    @Operation(summary = "Get the restaurants of a foodchain by ID")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Restaurants found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Restaurant.class)
+            )
+        )
+    })
     @GetMapping("/{foodchain_id}/restaurants")
-    public ResponseEntity<List<Restaurant>> getRestaurantsFromChain(@PathVariable(value = "foodchain_id") Long chainId) {
+    public ResponseEntity<List<Restaurant>> getRestaurantsFromChain(
+        @Parameter(description = "ID of the foodchain to retrieve restaurants", example = "1")
+        @PathVariable(value = "foodchain_id") Long chainId) {
         List<Restaurant> restaurants = restaurantsService.getRestaurantsFromChain(chainId);
         return new ResponseEntity<>(restaurants, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a restaurant of a foodchain by ID")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Restaurant found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Restaurant.class)
+            )
+        )
+    })
     @GetMapping("/{foodchain_id}/restaurants/{restaurant_id}")
-    public ResponseEntity<Restaurant> getRestaurantFromChainById(@PathVariable(value = "foodchain_id") Long chainId, @PathVariable(value = "restaurant_id") Long restId) {
+    public ResponseEntity<Restaurant> getRestaurantFromChainById(
+        @Parameter(description = "ID of the foodchain to retrieve the restaurant", example = "1")
+        @PathVariable(value = "foodchain_id") Long chainId, 
+        @Parameter(description = "ID of the restaurant to retrieve", example = "1")
+        @PathVariable(value = "restaurant_id") Long restId) {
         Restaurant res = restaurantsService.getRestaurantFromChainById(chainId, restId);
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get the menus of a foodchain by ID")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Menus found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Menu.class)
+            )
+        )
+    })
     @GetMapping("/{foodchain_id}/menus")
-    public ResponseEntity<List<Menu>> getMenusByChain(@PathVariable(value = "foodchain_id") Long chainId) {
+    public ResponseEntity<List<Menu>> getMenusByChain(
+        @Parameter(description = "ID of the foodchain to retrieve menus", example = "1")
+        @PathVariable(value = "foodchain_id") Long chainId) {
         List<Menu> menus = restaurantsService.getMenusByChain(chainId);
         return new ResponseEntity<>(menus, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get the order statistics of the foodchains")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Menu found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = OrderStatisticsDTO.class)
+            )
+        )
+    })
     @GetMapping("/orders/statistics")
     public ResponseEntity<Map<String, OrderStatisticsDTO>> getStatistics() {
         Map<String, OrderStatisticsDTO> stats = orderService.getAllStatistics();
         return new ResponseEntity<>(stats, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get the menu statistics of the foodchains")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Menu found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = Menu.class)
+            )
+        )
+    })
     @GetMapping("/menus/statistics")
     public ResponseEntity<List<Menu>> getMenuStatistics() {
         List<Menu> stats = orderService.getMenuStatistics();
         return new ResponseEntity<>(stats, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get the order statistics of a foodchain by ID")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Menu found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = OrderStatisticsDTO.class)
+            )
+        )
+    })
     @GetMapping("/{foodchain_id}/orders/statistics")
-    public ResponseEntity<Map<String, OrderStatisticsDTO>> getStatistics(@PathVariable(value = "foodchain_id") Long chainId) {
+    public ResponseEntity<Map<String, OrderStatisticsDTO>> getStatistics(
+        @Parameter(description = "ID of the foodchain to retrieve statistics", example = "1")
+        @PathVariable(value = "foodchain_id") Long chainId) {
         Map<String, OrderStatisticsDTO> stats = orderService.getStatisticsByChainId(chainId);
         return new ResponseEntity<>(stats, HttpStatus.OK);
     }
